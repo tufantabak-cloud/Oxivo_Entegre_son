0647cc94e897863b05ef2321bf13e020308f4a2f
tufantabak-cloud <tufan.tabak@gmail.com>
2025-11-17 19:55:14 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    // Eğer Oxivo_Entegre klasörü içinde de kodların varsa burayı da ekle:
-    "./Oxivo_Entegre/**/*.{js,ts,jsx,tsx}", 
+    "./**/*.{js,ts,jsx,tsx}", // <--- BU SATIR KRİTİK (Her yere bak demek)
   ],
   theme: {
     extend: {
