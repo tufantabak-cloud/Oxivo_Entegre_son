cef3c85e1fa8c084de558809a6fae452b5389116
tufantabak-cloud <tufan.tabak@gmail.com>
2025-12-04 12:48:29 +0300

    Update dialog.tsx

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -49,11 +49,13 @@ const DialogOverlay = React.forwardRef<
 
 DialogOverlay.displayName = "DialogOverlay";
 
-function DialogContent({
-  className,
-  children,
-  ...props
-}: React.ComponentProps<typeof DialogPrimitive.Content>) {
+function DialogContent(
+  {
+    className,
+    children,
+    ...props
+  }: React.ComponentProps<typeof DialogPrimitive.Content>
+) {
   return (
     <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
@@ -63,7 +65,6 @@ function DialogContent({
           "bg-white data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[100] grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border-0 p-6 shadow-2xl duration-200 sm:max-w-lg",
           className,
         )}
-        aria-describedby={undefined}
         {...props}
       >
         {children}
