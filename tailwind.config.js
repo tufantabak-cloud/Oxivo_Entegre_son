fefafff6d21dc9c34be2208a6f565b6bdfff7884
tufantabak-cloud <tufan.tabak@gmail.com>
2025-12-20 09:10:19 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     "./index.html",
     "./App.tsx",
     "./main.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./utils/**/*.{js,ts,jsx,tsx}"
   ],
